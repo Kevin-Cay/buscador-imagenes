87e571fbae54751a41c0f93a0ccb1b71e4f2a070
Kevin-Cay <56460566+Kevin-Cay@users.noreply.github.com>
2021-11-16 22:04:04 -0600

    reset component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {useState} from 'react'
 import {Formik, Form, Field} from 'formik'
+import refresh from './refresh.svg'
 import './Header.css'
 import './Content.css'
 import './Article.css'
@@ -7,7 +8,7 @@ import './Article.css'
 function App() {
   const [photos, setPhotos] = useState([]);
   const open = url => window.open(url)
-  const api = async (values) =>{
+  const api = async (values, {resetForm}) =>{
     const response = await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`, {
       headers:{
         'Authorization': 'Client-ID dWKsEoIGxek0aQWAnGXuyviWEK04cor_wPkrmv9wY88'
@@ -15,30 +16,34 @@ function App() {
     })
     const data = await response.json()
     setPhotos(data.results);
+    resetForm()
   }
-  const reset = () =>{
+  const handleReset = () =>{
     setPhotos([])
   }
+
+  // https://codesandbox.io/s/7122xmovnq?file=/src/index.js
   // console.log({photos});
   return (
     <div >
      <header>
+        
         <div>
-          <h1>Img</h1>
+              <h1>Img</h1>
         </div>
-
        <Formik
         initialValues={{search: ''}}
         onSubmit={api}
        >
-         <Form>
-           <label >Search: </label>
-           <Field name='search' />
-         </Form>
+          <Form>
+          <label >Search: </label>
+          <Field style={{fontSize: "16px"}} name='search' />
+        </Form>
+         
        </Formik>
        <div className='restart' >
-          <img onClick={reset} src="bx-refresh.svg" alt="" type='submit'/>
-       </div>
+              <img onClick={handleReset} src={refresh} alt="" type="reset" />
+          </div>
      </header>
      <div className="container">
        <div className="center">
